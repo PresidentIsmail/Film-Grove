ac8f64905e69f8851540dcb0960920551aecd7d0
Ismail <shaikhnagismail@gmail.com>
2023-08-03 18:11:28 +0200

    fix api key not defined

--- src/CustomHooks/useMovieSearch.js
+++ src/CustomHooks/useMovieSearch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const useMovieSearch = (query) => {
   const [movies, setMovies] = useState([]);
